9234a66bfd279278eda4a57ebaf750e1b637292d
Hugo Andrade <habp@ecomp.poli.br>
2022-09-27 14:55:41 -0300

    First commit, initializing development trace

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MudaMeuMundo_teste',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
