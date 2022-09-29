9715d74346f7f58330882e76f21f078ff8e693ed
Hugo Andrade <habp@ecomp.poli.br>
2022-09-29 11:35:19 -0300

    added untracked files

---
+++ src/pages/initmenu.tsx
@@ -0,0 +1,5 @@
+import { IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonPage, IonRow, IonTitle, IonToolbar, IonLabel, IonIcon, IonButton } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import { star, airplane, enter, personAdd } from 'ionicons/icons';
+import './Home.css';
+
